c353fbae03b4e9a12ff064eb91eb602f947b4459
Bimal <bimalvasan@gmail.com>
2015-06-17 02:30:20 -0500

    Added provider
    provider service demo

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,19 @@
-angular.module('app',[]);
+(function () {
+
+	angular.module('app', []);
+
+	angular.module('app').config(function ($provide) {
+		//Name of the provider function is 'books'.
+		$provide.provider('books', function () {
+			this.$get = function () {
+				var appName = 'Books Logger';
+				var appDesc = 'Track which books you read';
+
+				return {
+					appName: appName,
+					appDesc: appDesc
+				};
+			};
+		});
+	});
+}());
---
+++ public/app/books/BooksController.js
@@ -0,0 +1,10 @@
+(function () {
+	angular.module('app')
+		.controller('BooksController', BooksController);
+
+	function BooksController(books) {
+		var vm = this;
+
+		vm.appName = books.appName;
+	}
+}());
